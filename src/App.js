27e091c0f7657e3b800a7ac231a1b0de27d9a5ae
LucastaND <lucastand1112@gmail.com>
2023-10-25 22:41:32 +0700

    frontend-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component, useState } from "react";
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import SignupForm from "./components/SignupForm";
+import Home from "./page/Home";
+import ForgetForm from "./components/ForgetForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <SignupForm></SignupForm>
+
+        <Routes>
+          <Route path="/home" element={<Home />} />
+
+          <Route path="/login" element={<SignupForm />} />
+          <Route path="/forget" element={<ForgetForm />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
